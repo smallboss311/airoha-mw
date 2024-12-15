807674fc707e7e6c2388378409589969fd7d42ac
tuchuan <908041139@qq.com>
2024-12-15 10:46:08 +0800

    关闭mqtt队列中的消息打印

--- air_magic_wand/air_mw_system/mqttd/mqttd_queue.c
+++ air_magic_wand/air_mw_system/mqttd/mqttd_queue.c
@@ -243,7 +243,7 @@ mqttd_get_queue_recv(
         return rc;
     }
 
-    mqttd_debug_db("ptr_msg=%p", ptr_msg);
+    //mqttd_debug_db("ptr_msg=%p", ptr_msg);
     (*ptr_buf) = ptr_msg;
 
     return MW_E_OK;
@@ -561,7 +561,7 @@ mqttd_queue_getData(
     ptr_pload = (DB_PAYLOAD_T *)&(ptr_msg->ptr_payload);
     (*pptr_out_data) = &(ptr_pload->ptr_data);
 
-    mqttd_debug_db("*pptr_out_msg = %p, *pptr_out_data = %p \n", *pptr_out_msg, *pptr_out_data);
+    //mqttd_debug_db("*pptr_out_msg = %p, *pptr_out_data = %p \n", *pptr_out_msg, *pptr_out_data);
 
     return MW_E_OK;
 }
